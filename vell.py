c9f77e584996b2a0ac552273c88918d456db06b0
Jose Barranco <jdcoding01@gmail.com>
2018-02-08 19:02:10 -0400

    Update vell.py

--- vell.py
+++ vell.py
@@ -10,16 +10,15 @@ async def bot_online():
     print('Bot: ')
     print(bot.user.name)
 @bot.command()
-async def link():
+async def karmaguild():
     await bot.say("Karma Recruitment Center Invite Link: <https://discord.gg/BYc3Der>")
 @bot.command()
 async def beta():
     await bot.say("Beta phase, what else?!")
-@bot.command()
+@bot.comamnd()
 async def supportserver():
-   await bot.say("https://discord.gg/pMXFPbM")
-
-
+    await bot.say("https://discord.gg/pMXFPbM")
+        
 token = os.environ.get("TOKEN")
 bot.run(f'{token}')
 #bot link https://discordapp.com/oauth2/authorize?client_id=397217109035450368&scope=bot&permissions=0
