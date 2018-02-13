d2964314385cfd5e1c8d9c6e7f6c5626be24a0d5
Jose Barranco <jdcoding01@gmail.com>
2018-02-13 18:25:16 -0400

    Update vell.py

--- vell.py
+++ vell.py
@@ -10,11 +10,13 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import discord
 global misc_commands
-misc_commands = ["~beta", "~supportserver"]
+misc_commands = "~beta", "~supportserver"
 global command_help
 command_help = "For more info on a command type v.commandhelp where command is the name of it. Ex: v.betahelp"
 global music_commands
 music_commands = "In progress, not available yet..."
+global mod_commands
+mod_commands = "emoji", "text", "nsfw"
 bot = commands.Bot(command_prefix=prefix)
 bot.remove_command("help")
 
@@ -25,10 +27,11 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def help(ctx):
     embed = discord.Embed(title="Vell Bot Help Menu", description="Here you will find all the help you need. Not satisfied? Type join.supportserver!", color=0x00a0ea)
-    embed.set_thumbnail(url = "https://www.advancedgraphicsinc.com/letter-v-black-monogram-tumbler-decal.html")
+    embed.set_thumbnail(url = "https://www.google.com.do/search?q=help+png+logo&rlz=1CASMAI_enDO782DO782&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiyuPzz9qPZAhUpAcAKHaUKDM8Q_AUICigB&biw=1366&bih=629#imgrc=CdDEoMFiYlNvQM:")
     embed.add_field(name="Miscellaneous Commands".format("null"), value=misc_commands, inline=False)
-    embed.add_field(name="Music Commands".format("null"), value=music_commands)
+    embed.add_field(name="Music Commands".format("null"), value=music_commands, inline=False)
     embed.add_field(name="Commands Help".format("null"), value=command_help)
+    embed.add_field(name="Moderator Commands".format("null"), value=mod_commands)
     embed.set_footer(text="Vell Bot ~ Developed by Alphi#5113")
     await bot.say(embed=embed)
 @bot.command(pass_context=True)
