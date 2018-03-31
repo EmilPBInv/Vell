7948681c0b021aab39094e60aaa8ec618ba72b47
Jose Barranco <jdcoding01@gmail.com>
2018-03-31 14:52:28 -0400

    Update Music.py

--- Music.py
+++ Music.py
@@ -7,7 +7,7 @@ if not discord.opus.is_loaded():
     # you should replace this with the location the
     # opus library is located in and with the proper filename.
     # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+    discord.opus.load_opus('opus.dll')
 
 def __init__(self, bot):
         self.bot = bot
