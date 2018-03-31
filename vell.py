938d920d03df8cbf3d081b274389f87c6d6e3441
Jose Barranco <jdcoding01@gmail.com>
2018-03-31 19:08:11 -0400

    Update vell.py

--- vell.py
+++ vell.py
@@ -21,7 +21,7 @@ command_help = "For more info on a command type v.commandhelp where command is t
 global music_commands
 music_commands = ["In progress, not available yet..."]
 global mod_commands
-mod_commands = "emoji", "text", "nsfw", "kick"
+mod_commands = ["emoji", "text", "nsfw", "kick"]
 bot = commands.Bot(prefix=prefix)
 bot.remove_command("help")
 
